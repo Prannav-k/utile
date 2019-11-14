0dd6eac0094527e87ca25ce62502f2ea498b3372
Prannav-k <prannavk@gmail.com>
2019-11-15 01:08:20 +0530

    Actions for add strge,txn,update txn

---
+++ utile.cpp
@@ -0,0 +1,101 @@
+#include "middleware.cpp"
+
+
+void utile::login(name username){
+
+    require_auth(username);
+
+    //TODO any user , not just storages.
+
+    // Create a record in the table if the player doesn't exist in our app yet
+    auto storage_iterator = _storages.find(username.value);
+    if (storage_iterator == _storages.end()) {
+    storage_iterator = _storages.emplace(username,  [&](auto& new_storage) {
+      new_storage.storageName = username;
+    });
+  } 
+}
+
+
+void utile::addstorage(name storageName,uint32_t totalCapacity,uint32_t availableCapacity){
+
+  require_auth(storageName);
+
+  storage_table storages(get_self(), get_first_receiver().value);
+
+  auto iterator = storages.find(storageName.value);
+
+
+    if( iterator == storages.end() )
+    {
+      storages.emplace(storageName, [&]( auto& row ) {
+        row.storageName = storageName;
+        row.totalCapacity = totalCapacity;
+        row.availableCapacity = availableCapacity;
+      });
+    }
+}
+
+
+void utile::updatestorage(name storageName,uint32_t totalCapacity,uint32_t availableCapacity){
+
+  require_auth(storageName);
+
+  storage_table storages(get_self(), get_first_receiver().value);
+
+  auto iterator = storages.find(storageName.value);
+
+
+        check(iterator != storages.end(), "Record of given storage does not exist");
+
+   
+        storages.modify(iterator, storageName, [&]( auto& row ) {
+        row.storageName = storageName;
+        row.totalCapacity = totalCapacity;
+        row.availableCapacity = availableCapacity;
+      });
+}
+
+
+  // {
+
+  //           uint32_t     txnId;
+  //           name         storageName;
+  //           uint32_t     amount;
+  //           uint32_t     date;
+  //          // string&      categeory;
+  //           string       storageLocId; 
+  //           /* data */
+
+  //           auto primary_key() const {return txnId;}
+  //       };
+
+  void utile::addtxn(uint64_t txnId,name storageName, uint32_t amount, uint32_t date,string storageLocId ){
+
+  require_auth(storageName);
+
+  transaction_table transactions(get_self(), get_first_receiver().value);
+
+  auto iterator = transactions.find(txnId);
+
+    check(iterator == transactions.end(), "Record of given txn id already exist");
+
+
+    if( iterator == transactions.end() )
+    {
+      transactions.emplace(storageName, [&]( auto& row ) {
+        row.txnId = txnId;
+        row.storageName = storageName;
+        row.amount = amount;
+        row.date = date;
+        row.storageLocId = storageLocId;
+      });
+    }
+
+}
+
+
+
+
+EOSIO_DISPATCH(utile, (login)(addstorage)(updatestorage)(addtxn))
+
---
+++ utile.hpp
@@ -0,0 +1,75 @@
+#include <eosio/eosio.hpp>
+#include <string>
+
+
+using namespace eosio;
+using namespace std;
+//using std::string;
+
+
+
+class  [[eosio::contract("utile")]] utile: public eosio::contract{
+
+
+    private:
+
+        struct[[eosio::table]]  storage{
+
+            name            storageName;
+            uint32_t        totalCapacity;
+            uint32_t        availableCapacity;
+            //string&   address; 
+            
+            auto primary_key() const {return storageName.value; }
+
+        };
+
+        struct[[eosio::table]]  transaction
+        {
+
+            uint64_t     txnId;
+            name         storageName;
+            uint32_t     amount;
+            uint32_t     date;
+           // string&      categeory;
+            string       storageLocId; 
+            /* data */
+
+            auto primary_key() const {return txnId;}
+        };
+
+        /**
+         * A vector for supply chain?
+         */ 
+        
+
+        typedef eosio::multi_index<name("storages"), storage> storage_table;
+        typedef eosio::multi_index<name("transactions"),transaction> transaction_table;
+
+        storage_table _storages;
+        transaction_table _transasctions;
+
+
+    public:
+
+
+        utile( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds),
+                       _storages(receiver, receiver.value),
+                       _transasctions(receiver, receiver.value) {}
+
+        [[eosio::action]]
+        void login(name username);
+
+        [[eosio::action]]
+        void addstorage(name storageName,uint32_t totalCapacity,uint32_t availableCapacity);
+
+         [[eosio::action]]
+        void updatestorage(name storageName,uint32_t totalCapacity,uint32_t availableCapacity);
+
+        [[eosio::action]]
+        void addtxn(uint64_t txnId,name storageName, uint32_t amount, uint32_t date,string storageLocId );
+
+
+        // [[eosio::action]]
+        // void updatestorage();
+};
